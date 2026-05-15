22ceebc1f48afe4aa9edea8edc8b2d9d8063726a
kamaboko <79447012+KamabokoFish@users.noreply.github.com>
2026-05-16 00:55:12 +0900

    fix(treesitter): neovim12でtreesitterの仕様変更への対応など

--- lua/plugins/visual.lua
+++ lua/plugins/visual.lua
@@ -80,50 +80,37 @@ return {
 
   {
     'nvim-treesitter/nvim-treesitter',
-    build = ':TSUpdate',
+    branch = 'main',
     config = function()
-      local configs = require("nvim-treesitter.configs")
+      local parsers = {
+        "markdown",
+        "markdown_inline",
+        "html",
+        "typescript",
+        "javascript",
+        "java",
+        "css",
+        "lua",
+        "python",
+        "go",
+        "yaml",
+        "toml",
+        "json",
+        "rust",
+      }
 
-      configs.setup({
-        ensure_installed = { "c", "cpp", "vim", "dockerfile", "fish", "typescript", "tsx", "javascript", "json", "lua", "gitignore", "bash", "astro", "markdown", "css", "scss", "styled", "yaml", "toml", "vue", "php", "html", "vimdoc" },
-        highlight = {
-          enable = true,
-          additional_vim_regex_highlighting = false, -- catpuucin用
-          disable = {},
-        },
-        yati = {
-          enable = true,
-          -- Disable by languages, see `Supported languages`
-          disable = { "python" },
+      vim.api.nvim_create_user_command("TSInstallConfigured", function()
+        require("nvim-treesitter").install(parsers)
+      end, {})
 
-          -- Whether to enable lazy mode (recommend to enable this if bad indent happens frequently)
-          default_lazy = true,
-
-          -- Determine the fallback method used when we cannot calculate indent by tree-sitter
-          --   "auto": fallback to vim auto indent
-          --   "asis": use current indent as-is
-          --   "cindent": see `:h cindent()`
-          -- Or a custom function return the final indent result.
-          default_fallback = "auto"
-        },
-        indent = {
-          -- 一部のファイルでインデントが変になることがある
-          -- 一時的にインデントだけオフにして対応
-          enable = false,
-          -- disable ={"html"},
-        },
-        autotag = {
-          enable = true,
-          -- nvim-ts-autotagで奇妙な挙動になるのでfalseに
-          enable_close_on_slash = false
-        },
+      vim.api.nvim_create_autocmd("FileType", {
+        group = vim.api.nvim_create_augroup("vim-treesitter-start", {}),
+        callback = function(ctx)
+          -- 必要に応じて`ctx.match`に入っているファイルタイプの値に応じて挙動を制御
+          -- `pcall`でエラーを無視することでパーサーやクエリがあるか気にしなくてすむ
+          pcall(vim.treesitter.start)
+        end,
       })
-    end
-  },
-
-  { 'JoosepAlviste/nvim-ts-context-commentstring' }, -- context-comment with treesitter
-
-  { "yioneko/nvim-yati",                          version = "*" },
-
-  -- {'digitaltoad/vim-pug'}, -- for .jade,.pug file syntax
+    end,
+  }
 }
